a3266e8cb537a13ccabfa2087aea3083d1920968
Chris Jenkins <cdpjenkins@gmail.com>
2021-12-26 11:08:42 +0000

    Day 25 Implement south-facing cucumbers

--- src/main/kotlin/day25/Day25.kt
+++ src/main/kotlin/day25/Day25.kt
@@ -3,3 +3,70 @@ package day25
 fun main() {
 
 }
+
+fun Sequence<World>.iteration(i: Int) = elementAt(i).toPrintableString()
+fun worldsSequence(input: String) = generateSequence(worldFrom(input.trimIndent())) { it.move() }
+private fun worldFrom(input: String) = input.trimIndent().lines().parse()
+
+private fun List<String>.parse(): World {
+    val points = withIndex().flatMap { (y, line) ->
+        line.withIndex().map { (x, point) -> Point(x, y) to point }
+    }.toMap()
+    val width = this[0].length
+    val height = size
+
+    return World(points, width, height)
+}
+
+data class World(val points: Map<Point, Char>, val width: Int, val height: Int) {
+    fun toPrintableString() =
+        (0..height - 1).map { y ->
+            (0..width - 1).map { x ->
+                points[Point(x, y)]
+            }.joinToString("")
+        }.joinToString("\n")
+
+    fun move(): World {
+        val moveEast = moveEast()
+        val moveSouth = moveEast.moveSouth()
+        return moveSouth
+    }
+
+    private fun moveEast(): World {
+        val newPoints = (0..height - 1).flatMap { y ->
+            (0..width - 1).map { x ->
+                val value = pointAt(x, y)
+                val newValue = when (value) {
+                    '.' -> if (pointAt(x - 1, y) == '>') '>' else '.'
+                    '>' -> if (pointAt(x + 1, y) == '.') '.' else '>'
+                    'v' -> 'v'
+                    else -> '.'
+                }
+                Point(x, y) to newValue
+            }
+        }.toMap()
+
+        return World(newPoints, width, height)
+    }
+
+    private fun moveSouth(): World {
+        val newPoints = (0..height - 1).flatMap { y ->
+            (0..width - 1).map { x ->
+                val value = pointAt(x, y)
+                val newValue = when (value) {
+                    '.' -> if (pointAt(x, y - 1) == 'v') 'v' else '.'
+                    'v' -> if (pointAt(x, y + 1) == '.') '.' else 'v'
+                    '>' -> '>'
+                    else -> '.'
+                }
+                Point(x, y) to newValue
+            }
+        }.toMap()
+
+        return World(newPoints, width, height)
+    }
+
+    private fun pointAt(x: Int, y: Int) = points[Point(x.mod(width), y.mod(height))]
+}
+
+data class Point(val x: Int, val y: Int)
--- src/test/kotlin/day25/Day25KtTest.kt
+++ src/test/kotlin/day25/Day25KtTest.kt
@@ -18,7 +18,7 @@ internal class Day25KtTest {
     }
 
     @Test
-    internal fun `south facing cucumers move south`() {
+    internal fun `south facing cucumbers move south`() {
         val worldsSequence = worldsSequence(
             """
                 .v
@@ -32,47 +32,22 @@ internal class Day25KtTest {
             """.trimIndent()
         ))
     }
-}
 
-private fun Sequence<World>.iteration(i: Int) = drop(i).first().toPrintableString()
-private fun worldsSequence(input: String) = generateSequence(worldFrom(input.trimIndent())) { it.move() }
-private fun worldFrom(input: String) = input.trimIndent().lines().parse()
+    @Test
+    internal fun `south facing cucumbers wrap when they reach the southern edge of the world`() {
+        val worldsSequence = worldsSequence(
+            """
+                ..
+                .v
+            """
 
-private fun List<String>.parse(): World {
-    val points = withIndex().flatMap { (y, line) ->
-        line.withIndex().map { (x, point) -> Point(x, y) to point }
-    }.toMap()
-    val width = this[0].length
-    val height = size
-
-    return World(points, width, height)
-}
-
-data class World(val points: Map<Point, Char>, val width: Int, val height: Int) {
-    fun toPrintableString() =
-        (0..height - 1).map { y ->
-            (0..width - 1).map { x ->
-                points[Point(x, y)]
-            }.joinToString("")
-        }.joinToString("\n")
-
-    fun move(): World {
-        val newPoints = (0..height - 1).flatMap { y ->
-            (0..width - 1).map { x ->
-                val value = pointAt(x, y)
-                val newValue = when (value) {
-                    '.' -> if (pointAt(x - 1, y) == '>') '>' else '.'
-                    '>' -> if (pointAt(x + 1, y) == '.') '.' else '>'
-                    else -> '.'
-                }
-                Point(x, y) to newValue
-            }
-        }.toMap()
-
-        return World(newPoints, width, height)
+        )
+        assertThat(worldsSequence.iteration(1), equalTo(
+            """
+                .v
+                ..
+            """.trimIndent()
+        ))
     }
-
-    private fun pointAt(x: Int, y: Int) = points[Point(x.mod(width), y)]
 }
 
-data class Point(val x: Int, val y: Int)
