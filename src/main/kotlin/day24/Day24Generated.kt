4f2e96c9b31c76779c4fa76f8e60e1e53788f7f3
Chris Jenkins <cdpjenkins@gmail.com>
2021-12-28 17:44:25 +0000

    Day 24 finished manually

--- src/main/kotlin/day24/Day24Generated.kt
+++ src/main/kotlin/day24/Day24Generated.kt
@@ -1,11 +1,17 @@
 package day24
 
-fun doOneCell(input: Int, p1: Int, p2: Int, p3: Int, z: Int): Int =
-    if (z % 26 + p2 != input) {
+fun doOneCell(input: Int, p1: Int, p2: Int, p3: Int, z: Int): Int {
+    val x = z % 26 + p2
+    val predicate = x != input
+
+    println(String.format("input == %d z == %8d p1 == %2d p2 == %3d p3 = %3d x == %d %s", input, z, p1, p2, p3, x, predicate.toString()))
+
+    return if (predicate) {
         z / p1 * 26 + input + p3
     } else {
         z / p1
     }
+}
 
 fun makeCell(p1: Int, p2: Int, p3: Int) = { input: Int, z: Int -> doOneCell(input, p1, p2, p3, z) }
 
@@ -13,8 +19,9 @@ val cells = listOf(
     makeCell(1, 10, 12),
     makeCell(1, 10, 10),
     makeCell(1, 12, 8),
-    makeCell(1, 11, 6),
+    makeCell(1, 11, 4),
     makeCell(26, 0, 3),
+    makeCell(1, 15, 10),
     makeCell(1, 13, 6),
     makeCell(26, -12, 13),
     makeCell(26, -15, 8),
@@ -37,23 +44,19 @@ fun calculateTheThing(modelNumber: String): Int {
 }
 
 fun main() {
-    val startTime = System.currentTimeMillis()
-
-    for (i in 0..9999999999) {
-        val digits = i.toString().padStart(10, '0')
-        val modelNumber = "999999${digits}"
-        if (modelNumber.contains("0")) {
-            continue
-        }
-        val result = calculateTheThing(modelNumber)
-
-
-        if (result == 0) {
-            println("$modelNumber $result")
-            break
-        }
+    val modelNumber = "11815671117121"
+    val result = calculateTheThing(modelNumber)
+    if (result == 0) {
+        println("winner! $modelNumber $result")
     }
-
-    val timeTaken = System.currentTimeMillis() - startTime
-    println("Time taken: $timeTaken")
 }
+
+// Attempts for largest:
+// 93848993419899 - too low
+// 93859971419899
+// 93859993419899
+
+// 93959993429899 Is the winner (found with a bit of manual searching
+
+// Attempts for smallest:
+// 11815671117121 - is the winner. Again found manually.
